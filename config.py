7cb3da028825a24839d15c9727999a0516b91b5d
Victor Kolebanov <kolebanov@gmail.com>
2022-06-25 22:54:14 +0300

    add API and URL to config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 USE_ROUNDED_COORDS = True
+OPENWEATHER_API = "94d1bc1bccef5201e5bad6627af7b07b"
+OPENWEATHER_URL = (
+    "https://api.openweathermap.org/data/2.5/weather?"
+    "lat={latitude}&lon={longitude}&"
+    "appid=" + OPENWEATHER_API + "&lang=ru&"
+    "units=metric"
+)
